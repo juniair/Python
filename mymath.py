ee4bfced883fd8cd9049d4438194baa432b83c0f
juniair <juniair@naver.com>
2015-12-17 20:52:36 +0900

    수정본

---
+++ mymath.py
@@ -0,0 +1,9 @@
+import math
+
+def area(length):
+	return pow(length, 2) * math.pi
+
+if __name__ is "__main__":
+    print "area(5) = %f" % area(5)
+else:
+    print "It is not 'mymath' working space"
